f19365756b7696b53329c84dbf786e0c9fb3f220
Chao Qian <chao.qian@uni-due.de>
2020-06-03 12:53:40 +0200

    fixed the block issue of LUFA.

--- app/LufaWriteIntegrationTest.c
+++ app/LufaWriteIntegrationTest.c
@@ -8,19 +8,20 @@ int
 main(void)
 {
     uint16_t timer_1_sec=0;
+    char text[] = "hello, world!\r\n";
+
     DDRD = 0xff;
 
-    initLufa();
-    while(!lufaOutputAvailable())
+    initLufa();                   // Initialize LUFA
+    while(!lufaOutputAvailable()) // Blocked until LUFA available
     {
         _delay_ms(100);
     }
 
-    char text[] = "hello, world!\r\n";
     while (true)
     {
-        _delay_ms(1);
-        timer_1_sec++;
+
+        // Period task, blink the LED per 3 seconds
         if(timer_1_sec<=1500)
         {
             BitManipulation_setBit(&PORTD, PD4);
@@ -31,16 +32,32 @@ main(void)
         }else
         {
             timer_1_sec=0;
+            // once 3 seconds reached, send hello.
             lufaWriteString(text);
         }
-        lufaWaitUntilDone();
+
+        // receive buffer check, only check if the recv
+        // buffer is not empty.
         if(lufaReadAvailable())
         {
+            // receive buffer is empty, so read out 1 byte
             char read_char = lufaGetChar();
-            if (read_char=='a')
+            if (read_char=='a') // check if the key pressed is a
             {
                 lufaWriteString("I know you pressed key a.\r\n");
             }
+            else
+            {
+                lufaWriteString("\r\nPlease press the a on your keyboard.\r\n");
+            }
         }
+
+        // Time base for period task
+        _delay_ms(1);
+        timer_1_sec++;
+
+        // Lufa process, read and send happen in this process
+        lufaTask();
+
     }
 }
