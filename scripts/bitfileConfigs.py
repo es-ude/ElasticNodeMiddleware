ecb808a029f32b190af679acbb5c47f309e2229f
David Lewakis <david.lewakis@stud.uni-due.de>
2020-09-30 17:38:55 +0200

    Removed Local Path

--- scripts/bitfileConfigs.py
+++ scripts/bitfileConfigs.py
@@ -11,10 +11,7 @@ class BitfileConfigs:
 
     def __init__(self):
         # create a configuration for your bitfiles
-        # for bazel run you have to write the absolute path
-        # self.s15ConfigPart1 = Configuration("../bitfiles/s15_p1.bit", S15_ADDRESS_1, S15_ADDRESS_1)
-        # self.s15ConfigPart2 = Configuration("../bitfiles/s15_p2.bit", S15_ADDRESS_2, S15_ADDRESS_2)
 
         # change to your absolute path to the bitfiles!
-        self.s15ConfigPart1 = Configuration("/home/annika/projects-es/ElasticNodeMiddleware/bitfiles/s15_p1.bit", S15_ADDRESS_1, S15_ADDRESS_1)
-        self.s15ConfigPart2 = Configuration("/home/annika/projects-es/ElasticNodeMiddleware/bitfiles/s15_p2.bit", S15_ADDRESS_2, S15_ADDRESS_2)
+        self.s15ConfigPart1 = Configuration("../bitfiles/s15_p1.bit", S15_ADDRESS_1, S15_ADDRESS_1)
+        self.s15ConfigPart2 = Configuration("../bitfiles/s15_p2.bit", S15_ADDRESS_2, S15_ADDRESS_2)
