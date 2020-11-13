d28fc1f45d9c75b2a88c9a1ba243585d15fab369
David Lewakis <david.lewakis@stud.uni-due.de>
2020-11-13 16:58:42 +0100

    added error handling to init.py

--- templates/init.py
+++ templates/init.py
@@ -1,6 +1,10 @@
+import requests
+import sys
+
 def replacePorts(portTo):
-    r  = open("uploadScripts/portConfigs.py", "r")
-    f = r.read()
+    pC  = open("uploadScripts/portConfigs.py", "r")
+    f = pC.read()
+    pC.close()
     st = f.find("portTo" + portTo)
     if st != (-1):
         while f[st] != '"':
@@ -16,19 +20,27 @@ def replacePorts(portTo):
         if newText != f:
             print("Port to " + portTo + " set in uploadScripts/portConfigs.py")
         w.close()
-        r.close()
         if portTo == "Programmer":
             r = open("user.bazelrc", "r")
             f = r.read()
+            r.close()
             w = open("user.bazelrc", "w")
             newText = f.replace(current,newPort)
-            w.write(newText)
             if newText != f:
+                w.write(newText)
                 print("Port to " + portTo + " set in user.bazelrc")
+            else:
+                if input("Could not find port " + portTo + " in user.bazelrc. Load default user.bazelrc? (y/N) (default N) ") == "y" or "yes":
+                    w.write("run -- " + newPort)
+                    print("Loaded default user.bazelrc and set port to " + portTo + ".")
             w.close()
-            r.close()
     else:
-        print("Could not find port to " + portTo)
+        if input("Could not find port to " + portTo + ". Load default portConfigs? (y/N) (default N) ") == "y" or "yes":
+            w = open("uploadScripts/portConfigs.py", "w")
+            w.write(requests.get("https://raw.githubusercontent.com/es-ude/ElasticNodeMiddleware/master/templates/portConfigs.py").text)
+            w.close
+            print("Successful, run again to change ports.")
+            sys.exit()
 
 replacePorts("Programmer")
 replacePorts("Elasticnode")
