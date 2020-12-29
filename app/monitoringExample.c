a23acd960fe075857ff24bd56ea04f25695c04e0
David Lewakis <david.lewakis@stud.uni-due.de>
2020-12-29 20:17:01 +0100

    change to new measurement code

--- app/monitoringExample.c
+++ app/monitoringExample.c
@@ -4,68 +4,49 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#include "src/debug/debug.h"
 #include "src/led/led_mcu.h"
 #include "src/sub-system/sub-system.h"
 
 #include <compat/twi.h>
 
-void IIC_slave_init(uint8_t address);
 uint8_t system_state = 0;
-extern uint8_t state_of_the_mcu;
-
 int main(void)
 {
     IIC_slave_init(MY_IIC_ADDRESS);
-    uint16_t timer_1_sec=0;
-    led_mcu_init();
-    state_of_the_mcu = 0;
 
-    // init debug --> debugging with lufa via USB
-    debugInit(NULL);
+    led_mcu_init();
 
     while (true) {
 
-        _delay_ms(10000);
+        //start delay
+        _delay_ms(5000);
 
         led_mcu_turnOn(0);
-        change_state(RECONFIG_1, &state_of_the_mcu);
-        change_sample_rate(0,&state_of_the_mcu);
+        change_sample_rate(CURRENT_SAMPLE_TIME_2ms5);
+        change_running_state(RECONFIG_1);
         _delay_ms(1000);
+
+
         led_mcu_turnOn(1);
-        //state_of_the_mcu = 2;
+        change_sample_rate(CURRENT_SAMPLE_TIME_10ms);
+        change_running_state(SEND_INPUT_1);
+        _delay_ms(1000);
 
-        change_state(SEND_INPUT_1, &state_of_the_mcu);
-        change_sample_rate(4, &state_of_the_mcu);
-        _delay_ms(500);
         led_mcu_turnOn(2);
-        //state_of_the_mcu = 3;
-
-        change_state(EXECUTION_1, &state_of_the_mcu);
-        change_sample_rate(0, &state_of_the_mcu);
+        change_sample_rate(CURRENT_SAMPLE_TIME_80ms);
+        change_running_state(EXECUTION_1);
         _delay_ms(1000);
+
         led_mcu_turnOn(3);
-        //state_of_the_mcu = 4;
-
-        change_state(RECEIVE_RES_1, &state_of_the_mcu);
-        change_sample_rate(0, &state_of_the_mcu);
+        change_sample_rate(CURRENT_SAMPLE_TIME_160ms);
+        change_running_state(RECEIVE_RES_1);
         _delay_ms(1000);
+
         led_mcu_turnOffAll();
-        //state_of_the_mcu = 0;
+        change_running_state(SLEEP_MODE);
+        change_sample_rate(1);
 
-        change_state(SLEEP_MODE, &state_of_the_mcu);
-        change_sample_rate(1,&state_of_the_mcu);
-        _delay_ms(1000);
     }
 
-
     return 0;
-}
-
-void IIC_slave_init(uint8_t address){
-    cli();
-    //load address into TWI address register
-    TWAR = address;
-    TWCR = (1<<TWIE) | (1<<TWEA) | (1<<TWINT) | (1<<TWEN);
-    sei();
 }
--- src/sub-system/sub-system.h
+++ src/sub-system/sub-system.h
@@ -6,64 +6,58 @@
 
 #define MY_IIC_ADDRESS 64
 
+#define TEMPORAL_ACCELERATOR 1
+
 #include <stdint.h>
 
 //uint8_t state_of_the_mcu;
 //uint8_t sample_rate;
 
+#if TEMPORAL_ACCELERATOR
 typedef enum {
-            SLEEP_MODE = 0,
-            RECONFIG_1 = 1, 
-            SEND_INPUT_1 = 2,
-            EXECUTION_1 = 3,
-            RECEIVE_RES_1 = 4,
-            RECONFIG_2 = 5, 
-            SEND_INPUT_2 = 6,
-            EXECUTION_2 = 7,
-            RECEIVE_RES_2 = 8,
-            RECONFIG_3 = 9, 
-            SEND_INPUT_3 = 10,
-            EXECUTION_3 = 11,
-            RECEIVE_RES_3 = 12,
-            RECONFIG_4 = 13, 
-            SEND_INPUT_4 = 14,
-            EXECUTION_4 = 15,
-            RECEIVE_FINAL_RES = 16,
+    SLEEP_MODE = 0,
+    RECONFIG_1 = 1,
+    SEND_INPUT_1 = 2,
+    EXECUTION_1 = 3,
+    RECEIVE_RES_1 = 4,
+    RECONFIG_2 = 5,
+    SEND_INPUT_2 = 6,
+    EXECUTION_2 = 7,
+    RECEIVE_RES_2 = 8,
+    RECONFIG_3 = 9,
+    SEND_INPUT_3 = 10,
+    EXECUTION_3 = 11,
+    RECEIVE_RES_3 = 12,
+    FREE_TO_CHANGE1 = 13,
+    FREE_TO_CHANGE2 = 14,
+    END_MEASUREMENT = 15,
 
-}mcu_state;
+}mcu_running_state;
+#endif
 
+typedef enum{
 
-// typedef enum{
+    CURRENT_SAMPLE_TIME_2ms5,
+    CURRENT_SAMPLE_TIME_5ms,
+    CURRENT_SAMPLE_TIME_10ms,
+    CURRENT_SAMPLE_TIME_20ms,
+    CURRENT_SAMPLE_TIME_40ms,
+    CURRENT_SAMPLE_TIME_80ms,
+    CURRENT_SAMPLE_TIME_160ms,
+    CURRENT_SAMPLE_TIME_320ms,
+    CURRENT_SAMPLE_TIME_DEFAULT=5
 
+}sample_rate;
 
-// }sample_rate;
+#if TEMPORAL_ACCELERATOR
+void change_running_state(mcu_running_state new_state);
+#else
 
+void change_running_state(uint8_t new_state, uint8_t *state_of_the_mcu);
+#endif
 
-void change_state(mcu_state new_state, uint8_t *state_of_the_mcu);
+void change_sample_rate(sample_rate new_sample_rate);
 
-void change_sample_rate(uint8_t new_sample_rate, uint8_t *state_of_the_mcu);
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void IIC_slave_init(uint8_t address);
 
 #endif
