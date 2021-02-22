f6942adbd1dba78e3312737701474ce254da2593
David Lewakis <david.lewakis@stud.uni-due.de>
2021-02-22 11:58:48 +0100

    blinkLufaExample update

--- app/blinkLufaExample.c
+++ app/blinkLufaExample.c
@@ -5,10 +5,9 @@
 
 #include "ElasticNodeMiddleware/ElasticNodeMiddleware.h"
 
-int main(void)
-{
+int main(void) {
 
-    uint16_t timer_1_sec=0;
+    uint16_t timer_1_sec = 0;
     led_mcu_init();
 
     // init debug --> debugging with lufa via USB
@@ -17,32 +16,25 @@ int main(void)
     while (true) {
 
         // Period task, blink the LED per 3 seconds
-        if(timer_1_sec<=1500)
-        {
+        if (timer_1_sec <= 1500) {
             led_mcu_turnOn(0);
-        }
-        else if(timer_1_sec<=3000)
-        {
+        } else if (timer_1_sec <= 3000) {
             led_mcu_turnOff(0);
-        }else
-        {
-            timer_1_sec=0;
+        } else {
+            timer_1_sec = 0;
             // once 3 seconds reached, send hello.
             debugWriteLine("Hello. You debug with Lufa.");
         }
 
         // receive buffer check, only check if the recv
         // buffer is not empty.
-        if(debugReadCharAvailable())
-        {
+        if (debugReadCharAvailable()) {
             // receive buffer is empty, so read out 1 byte
             char read_char = debugGetChar();
-            if (read_char=='a') // check if the key pressed is a
+            if (read_char == 'a') // check if the key pressed is a
             {
                 debugWriteLine("I know you pressed key a.");
-            }
-            else
-            {
+            } else {
                 debugWriteLine("Please press the a on your keyboard.");
             }
         }
@@ -54,7 +46,6 @@ int main(void)
         // Lufa process, read and send happen in this process
         debugTask();
     }
-
 
     return 0;
 }
