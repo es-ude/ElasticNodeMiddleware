cffbd7ac78a7894a17c8f78464c4b452da444163
David Lewakis <david.lewakis@stud.uni-due.de>
2021-03-26 12:42:20 +0100

    example files update for new projekt structure

--- app/blinkExample.c
+++ app/blinkExample.c
@@ -5,29 +5,29 @@
 
 int main(void) {
 
-    led_mcu_init();
-    led_mcu_turnOnAll();
+    elasticnode_led_mcu_init();
+    elasticnode_led_mcu_turnOnAll();
     _delay_ms(500);
-    led_mcu_turnOffAll();
+    elasticnode_led_mcu_turnOffAll();
     _delay_ms(500);
 
     while (true) {
-        led_mcu_turnOn(0);
+        elasticnode_led_mcu_turnOn(0);
         _delay_ms(500);
-        led_mcu_turnOn(1);
+        elasticnode_led_mcu_turnOn(1);
         _delay_ms(500);
-        led_mcu_turnOn(2);
+        elasticnode_led_mcu_turnOn(2);
         _delay_ms(500);
-        led_mcu_turnOn(3);
+        elasticnode_led_mcu_turnOn(3);
         _delay_ms(500);
 
-        led_mcu_turnOff(0);
+        elasticnode_led_mcu_turnOff(0);
         _delay_ms(500);
-        led_mcu_turnOff(1);
+        elasticnode_led_mcu_turnOff(1);
         _delay_ms(500);
-        led_mcu_turnOff(2);
+        elasticnode_led_mcu_turnOff(2);
         _delay_ms(500);
-        led_mcu_turnOff(3);
+        elasticnode_led_mcu_turnOff(3);
         _delay_ms(500);
 
     }
--- app/blinkLufaExample.c
+++ app/blinkLufaExample.c
@@ -8,34 +8,34 @@
 int main(void) {
 
     uint16_t timer_1_sec = 0;
-    led_mcu_init();
+    elasticnode_led_mcu_init();
 
     // init debug --> debugging with lufa via USB
-    debugInit(NULL);
+    elasticnode_debugInit(NULL);
 
     while (true) {
 
         // Period task, blink the LED per 3 seconds
         if (timer_1_sec <= 1500) {
-            led_mcu_turnOn(0);
+            elasticnode_led_mcu_turnOn(0);
         } else if (timer_1_sec <= 3000) {
-            led_mcu_turnOff(0);
+            elasticnode_led_mcu_turnOff(0);
         } else {
             timer_1_sec = 0;
             // once 3 seconds reached, send hello.
-            debugWriteLine("Hello. You debug with Lufa.");
+            elasticnode_debugWriteLine("Hello. You debug with Lufa.");
         }
 
         // receive buffer check, only check if the recv
         // buffer is not empty.
-        if (debugReadCharAvailable()) {
+        if (elasticnode_debugReadCharAvailable()) {
             // receive buffer is empty, so read out 1 byte
-            char read_char = debugGetChar();
+            char read_char = elasticnode_debugGetChar();
             if (read_char == 'a') // check if the key pressed is a
             {
-                debugWriteLine("I know you pressed key a.");
+                elasticnode_debugWriteLine("I know you pressed key a.");
             } else {
-                debugWriteLine("Please press the a on your keyboard.");
+                elasticnode_debugWriteLine("Please press the a on your keyboard.");
             }
         }
 
@@ -44,7 +44,7 @@ int main(void) {
         timer_1_sec++;
 
         // Lufa process, read and send happen in this process
-        debugTask();
+        elasticnode_debugTask();
     }
 
     return 0;
